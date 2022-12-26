022e0ad834586675f161de4721669290324501bb
Samson <66468924+SamsonIdowu@users.noreply.github.com>
2022-12-27 00:32:06 +0300

    v1

---
+++ webdata/webapp.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body style="background-color: yellow">
+        <h1>DevSecOps tool chain project by Samson, Ayomide, Rolly and Emeka is successful!!!</h1>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
