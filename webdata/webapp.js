085768d98d0bd9b8bb382d569c0db329e391c042
Samson <66468924+SamsonIdowu@users.noreply.github.com>
2023-01-02 18:10:56 +0100

    v2

--- webdata/webapp.js
+++ webdata/webapp.js
@@ -3,11 +3,23 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <html>
-      <body style="background-color: yellow">
-        <h1>DevSecOps tool chain project by Samson, Ayomide, Rolly and Emeka is successful!!!</h1>
-      </body>
-    </html>
+  <html>
+  <head>
+  <title> DevSecOps Tool Chain Project (RP1) </title>
+  </head>
+
+    <body style="background-color:Cornsilk;">  
+    <h1> DevSecOps Tool Chain Project (RP1) </h1>
+      <h3 style="background-color:SlateBlue;">This project was done by Samson, Ayomide, Rolly and Emeka.</h3>
+
+      <p> Resources used are located at: </p>
+      <p> Jenkins Master: 10.1.1.41 </p>
+      <p> Webserver: 10.1.1.40 </p>
+      <p> Domain Contoller: 10.1.1.50 </p>
+      <p> VMware ESXi server: 10.1.1.210 </p>
+      
+    </body>
+  </html>
   `);
 });
 
